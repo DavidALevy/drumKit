541d1d69c38b61cdc9f7cacc4456a7ac18eb692e
David Levy <david.levy.levy@btinternet.com>
2021-11-07 19:42:10 +0000

    Update apps1.js

--- apps1.js
+++ apps1.js
@@ -1,57 +1,33 @@
 
-var scoreCount=0;
-var clickCount=0;
-
-const button=document.getElementById("clik");
-button.addEventListener("click",()=>{
-    let random=(Math.floor(Math.random() * 6) + 1);
-    
-    if(clickCount==0)
-    {
-    document.getElementById('dice').innerHTML=('DICE');
-    document.getElementById('score').innerHTML=('score: 0');
-    document.getElementById('clik').innerHTML=('Roll');
-    document.getElementById('status').innerHTML=('');
-    clickCount++   ;
-    scoreCount=0;
-    return;
-    }
-    else if(clickCount>0 &&scoreCount+random>=20 &&random!=1){
-        document.getElementById('dice').innerHTML=(`DICE value is ${random}`);
-        document.getElementById('score').innerHTML=(`score: ${scoreCount+random}`);            
-        document.getElementById('status').innerHTML=('You win!');
-              scoreCount=0; 
-              clickCount=0;   
-              return; 
-            }
-    else if  (clickCount>0 &&random!==1 &&scoreCount+random<20)
-  
-  {scoreCount=scoreCount+random;
-     
-    document.getElementById('dice').innerHTML=(`DICE value is ${random}`);
- document.getElementById('score').innerHTML=(`score: ${scoreCount}`);
- document.getElementById('clik').innerHTML=('Roll');
- 
-clickCount++
-return;
- } 
-
-else if(clickCount>0 &&random==1){
-    scoreCount=scoreCount+random
-    document.getElementById('dice').innerHTML=('DICE value is 1');
-    document.getElementById('score').innerHTML=('score:0');
-    document.getElementById('status').innerHTML=('You lose!');
-          scoreCount=0; 
-          clickCount=0;   
-          return; 
-        }
-        
-        
-
- 
+let drums=[
+['qKey','boom',81],
+['wKey','clap',87],
+['eKey','hihat',69],
+['rKey','kick',82],
+['tKey','openhat',84],
+['yKey','ride',89],
+['uKey','snare',85],
+['iKey','tink',73],
+['oKey','tom',79]
+];
 
 
- 
-}
-)
+for(let i=0;i<drums.length;i++){
+    document.getElementById(drums[i][0]).addEventListener("click", () => {
+    document.getElementById(drums[i][1]).pause();
+    document.getElementById(drums[i][1]).currentTime = 0;
+    document.getElementById(drums[i][1]).play();
+})}
+
+
+for(let i=0;i<drums.length;i++){
+document.addEventListener("keydown",function(event){
+   if( event.which ==drums[i][2])    
+                {document.getElementById(drums[i][1]).pause();
+                        document.getElementById(drums[i][1]).currentTime = 0;
+                        document.getElementById(drums[i][1]).play()};
+                        {return 0};
+
+                })}
+
 
